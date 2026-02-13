eca3f8b68d7fecd93611e48d137f916ba9e1ba4e
Mohammed Aliwi <70952834+mhmad707@users.noreply.github.com>
2026-02-13 17:46:33 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, ".", "");
 
   return {
-    base: "/lidarboard/", // 🔴 MUST match GitHub repo name
+    base: "/Leidarbord/", // 🔴 MUST match GitHub repo name
     plugins: [react()],
     server: {
       port: 3000,
